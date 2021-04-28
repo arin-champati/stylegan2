0cf07d7c6979474f502fc0f56fef8dd792d7ff68
Arin Champati <champati.arin@gmail.com>
2021-04-28 11:06:24 -0400

    Update pretrained_networks.py

--- pretrained_networks.py
+++ pretrained_networks.py
@@ -59,12 +59,8 @@ def get_path_or_url(path_or_gdrive_path):
 
 #----------------------------------------------------------------------------
 
-_cached_networks = dict()
-
 def load_networks(path_or_gdrive_path):
     path_or_url = get_path_or_url(path_or_gdrive_path)
-    if path_or_url in _cached_networks:
-        return _cached_networks[path_or_url]
 
     if dnnlib.util.is_url(path_or_url):
         stream = dnnlib.util.open_url(path_or_url, cache_dir='.stylegan2-cache')
@@ -74,7 +70,6 @@ def load_networks(path_or_gdrive_path):
     tflib.init_tf()
     with stream:
         G, D, Gs = pickle.load(stream, encoding='latin1')
-    _cached_networks[path_or_url] = G, D, Gs
     return G, D, Gs
 
 #----------------------------------------------------------------------------
